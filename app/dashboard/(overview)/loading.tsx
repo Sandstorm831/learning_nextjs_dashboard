259845ab423514087954f2874401c6d56f31afd4
rohan <gargrohan138@gmail.com>
2024-12-29 00:55:52 +0530

    done search and pagination

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from "../../ui/skeletons";
+
+// This component will load into the child prop in layout page untill any async function is ready for rendering.
+export default function Loading(){
+    return <DashboardSkeleton />
+}
